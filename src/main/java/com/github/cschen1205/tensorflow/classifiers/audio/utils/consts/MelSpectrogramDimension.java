63d2438bb967219849628f8ba59f28d249ac1d60
xschen <cschen1205@gmail.com>
2018-04-02 13:08:51 +0800

    source upload

---
+++ src/main/java/com/github/cschen1205/tensorflow/classifiers/audio/utils/consts/MelSpectrogramDimension.java
@@ -0,0 +1,6 @@
+package com.github.cschen1205.tensorflow.classifiers.audio.utils.consts;
+
+public class MelSpectrogramDimension {
+    public static final int Width = 1366;
+    public static final int Height = 96;
+}
